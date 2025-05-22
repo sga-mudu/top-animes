2b203fd0ca29cafc466209d72b31b9e9fc3cbf7b
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-05-22 17:11:18 +0800

    hopelastt

--- javascript files/details.js
+++ javascript files/details.js
@@ -71,7 +71,7 @@ export function backButton(){
     const backBtn = document.querySelector(".back-btn");
     if(backBtn){
         backBtn.addEventListener("click", ()=>{
-            location.reload();
+            window.location.reload();
             window.history.back();
         });
     }
